78d9ac9c208ad76ed7ff58909efb38451a216aac
naditya98 <kumardityaapna29@gmail.com>
2021-02-25 09:42:12 +0530

    Fixxx Lastt Committ

--- src/dist/firebase.dev.js
+++ src/dist/firebase.dev.js
@@ -16,14 +16,14 @@ require("firebase/database");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var firebaseApp = _app["default"].initializeApp({
-  apiKey: "AIzaSyBvkUMaV-U3zLS32we1Gpxc2i9eIseb3Ac",
-  authDomain: "mumble-f5b70.firebaseapp.com",
-  databaseURL: "https://mumble-f5b70-default-rtdb.firebaseio.com",
-  projectId: "mumble-f5b70",
-  storageBucket: "mumble-f5b70.appspot.com",
-  messagingSenderId: "595927879007",
-  appId: "1:595927879007:web:d39701c23e30aedda0faea",
-  measurementId: "G-WD2GDSWNWW"
+  apiKey: "AIzaSyCykLI8fjtlMnz4ENaNkbpJMiW6jHL5MY8",
+  authDomain: "final-8ffe0.firebaseapp.com",
+  databaseURL: "https://final-8ffe0-default-rtdb.firebaseio.com",
+  projectId: "final-8ffe0",
+  storageBucket: "final-8ffe0.appspot.com",
+  messagingSenderId: "321294050630",
+  appId: "1:321294050630:web:dd4c5914296a439649f147",
+  measurementId: "G-N4RCXJC7E3"
 });
 
 var db = firebaseApp.firestore();
